67c21d02e6001d5404d4bd649608502727fb2db6
EmilJorgensen <Emil-Jorgensen@live.dk>
2017-04-19 12:10:54 +0200

    JDBC Creator and Connector Class
    A general connector class and creator class created. Creation of dbGAME
    setup

--- src/controllers/Game.java
+++ src/controllers/Game.java
@@ -1,5 +1,51 @@
 package controllers;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import controllers.dbCreator.tbCreator;
+
 public class Game {
 
+	public static void main(String[] argm){
+		dbCreator Creator = new dbCreator();
+			Creator.DeleteGameTemp();
+			
+			//CREATION
+			Creator.CreateGame();
+			tbCreator.tbCreatorGame();
+			
+			//CONNECTION
+		dbConnector Connector = new dbConnector("Localhost", 3306, "game", "root", "sql123");
+			if(Connector!=null)System.out.println("Connection sucessful...");
+			
+			//INSERTION
+			try {
+				Connector.doUpdate("INSERT into PLAYER values(1, 'Simon', 'Red', 11, 0);");
+			} catch (SQLException e) {
+				
+				e.printStackTrace();
+			}
+			
+			//VIEWING
+			try {
+				ResultSet rs = Connector.doQuery("SELECT * FROM player");
+				while(rs.next()){
+					String n = rs.getString("name");
+					int j = rs.getInt("GetOutOfJail");
+					System.out.println(n + "\t" + j);
+
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+			//CLOSURE
+			try {
+				Connector.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+	}
 }
