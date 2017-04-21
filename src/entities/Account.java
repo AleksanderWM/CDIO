b268c278e550392a8da35110ebed785c9a0c511c
SimonChr <simonchristiansen1@gmail.com>
2017-04-21 15:52:10 +0200

    Revert "ups"
    This reverts commit d4ae588071e7548949014da8782cf8110178b116.

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -1,74 +1,26 @@
 package entities;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import controllers.DBconnector;
-
 public class Account {
 	
-	DBconnector connector = new DBconnector();
+	private int balance;
 
-	public Account(int ID){
-		try{
-		connector.doUpdate("game","INSERT into ACCOUNT values(" + ID + "," + 30000 + "," + 30000 + ");");
-		}
-		 catch (SQLException e) {
-				e.printStackTrace();
-			}
+	public Account(){
+		balance = 30000;
 	}
 	
-	public int getMoney(int ID){
-		connector.Connect("game");
-		int Moneys = 0;
-		try {
-			ResultSet rs = connector.doQuery("Game","SELECT Money FROM ACCOUNT WHERE "+ ID +"EQUALS PlayerID");
-			while(rs.next()){
-				int Money = rs.getInt("Money");
-				connector.close();
-				Moneys = Money;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return Moneys;
+	public int getBalance(){
+		return balance;
 	}
 	
-	public void addBalance(int ID, int money){
-		connector.Connect("game");
-		int newBalance = money + getMoney(ID);
-		try {
-			connector.doUpdate("Game","UPDATE ACCOUNT SET Money = " + newBalance + " WHERE PlayerID EQUALS " + ID + ");");
-				connector.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+	public void setBalace(int balance){
+		this.balance = balance;
 	}
 	
-	public void setBalance(int ID, int balance){
-		connector.Connect("game");
-		try {
-			connector.doUpdate("Game","UPDATE ACCOUNT SET Money = " + balance + " WHERE PlayerID EQUALS " + ID + ");");
-				connector.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+	public void addBalance(int balance){
+		this.balance = this.balance + balance;
 	}
 	
-	public boolean enoughMoney(int ID, int price){
-			connector.Connect("game");
-			int Moneys = 0;
-			try {
-				ResultSet rs = connector.doQuery("Game","SELECT Money FROM ACCOUNT WHERE "+ ID +"EQUALS PlayerID");
-				while(rs.next()){
-					int Money = rs.getInt("Money");
-					connector.close();
-					Moneys = Money;
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			return Moneys > price;
-		
+	public boolean enoughMoney(int price){
+		return balance >= price;
 	}
 }
