2ef3ed0472a771d26dc34674c801b49f7e54b120
Aleksander <aleksander.wm@gmail.com>
2017-04-21 14:43:48 +0200

    Game and Playturn
    Created the basis of the classes.

--- src/controllers/PlayTurn.java
+++ src/controllers/PlayTurn.java
@@ -3,12 +3,19 @@ package controllers;
 import entities.Player;
 
 public class PlayTurn implements Runnable{
-	Player player = new Player();
-	int count;
+	Game game = new Game();
+	
+	int count = game.playerCounter;
 	@Override
 	public void run() {
+		String name = mui.getUserString();
+		Player player = new Player(name, count, null );
+		
 		while(player.getAccount().getBalance()!=0){
-			while(count==player.getNumber()); //Waiting for number to be changed to their turn.
+			while(count==player.getID()){
+				//Waiting for number to be changed to their turn.
+				
+			}
 				
 			//this is where the code goes for the game.
 			System.out.println("Boya");
