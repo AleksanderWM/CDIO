ab29a37cfa945331acdf0f655da86e3ce6ccca02
SimonChr <simonchristiansen1@gmail.com>
2017-05-02 15:21:42 +0200

    Player
    Get methods now return local values
    Objects now have all its values local and in db
    Only set methods and constructer use db

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -6,7 +6,6 @@
 
 package entities;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import controllers.DBconnector;
@@ -19,15 +18,17 @@ public class Player {
 	private Account Account;
 	private int getOutOfJail = 0;
 	private int ID = 0;
+	private String name;
 	
 	private int maxfields;
 	DBconnector connector = new DBconnector();
 	
 	public Player(String name, int ID){
-		try {
-			connector.doUpdate("game","INSERT into PLAYER values(" + ID + ",'" + name +"', " + position + ", " + getOutOfJail +");");
 			this.Account = new Account(ID);
 			this.ID = ID;
+			this.name = name;
+		try {
+			connector.doUpdate("game","INSERT into PLAYER values(" + ID + ",'" + name +"', " + position + ", " + getOutOfJail +");");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -35,41 +36,19 @@ public class Player {
 	}
 	
 	public String getName(){
-		connector.Connect("game");
-		String playername = null;
-		try {
-			ResultSet rs = connector.doQuery("Game","SELECT name FROM PLAYER WHERE "+ ID +"EQUALS PlayerID");
-			while(rs.next()){
-				String names = rs.getString("Name");
-				connector.close();
-				playername = names;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return playername;
+		return name;
 	}
 	
 	public int getOutOfJail(){
-		connector.Connect("game");
-		int amountOfFreecards = 0;
-		try {
-			ResultSet rs = connector.doQuery("Game","SELECT name FROM PLAYER WHERE "+ ID +"EQUALS PlayerID");
-			while(rs.next()){
-				int freecards = rs.getInt("GetOutOfJail");
-				connector.close();
-				amountOfFreecards = freecards;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return amountOfFreecards;
+	return getOutOfJail;
 	}
 	
 	public void setOutOfJail(int Amount){
 		connector.Connect("game");
+		int goojNow = getOutOfJail + Amount;
+		getOutOfJail = goojNow;
 		try {
-			connector.doUpdate("Game","UPDATE Player SET GetOutOfJail = " + getOutOfJail() + Amount + " WHERE PlayerID EQUALS " + ID + ");");
+			connector.doUpdate("Game","UPDATE Player SET GetOutOfJail = " + goojNow + " WHERE PlayerID EQUALS " + ID + ");");
 				connector.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -77,23 +56,12 @@ public class Player {
 	}
 	
 	public int getPosition(){
-		connector.Connect("game");
-		int playerPosition = 0;
-		try {
-			ResultSet rs = connector.doQuery("Game","SELECT position FROM PLAYER WHERE "+ ID +"EQUALS PlayerID");
-			while(rs.next()){
-				int position = rs.getInt("Position");
-				connector.close();
-				playerPosition = position;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return playerPosition;
+		return position;
 	}
 	
 	public void setPosition(int newPosition){
 		connector.Connect("game");
+		position = newPosition;
 		try {
 			connector.doUpdate("Game","UPDATE Player SET Position = " + newPosition + " WHERE PlayerID EQUALS " + ID + ");");
 				connector.close();
@@ -104,11 +72,12 @@ public class Player {
 	
 	public void movePosition(int moves){
 		connector.Connect("game");
-		int newPosition = moves + getPosition();
+		int newPosition = moves + position;
 		if(newPosition > maxfields){
 			newPosition = newPosition % maxfields;
 			Account.addBalance(ID, 4000);
 		}
+		position = newPosition;
 		try {
 			connector.doUpdate("Game","UPDATE Player SET Position = " + newPosition + " WHERE PlayerID EQUALS " + ID + ");");
 				connector.close();
