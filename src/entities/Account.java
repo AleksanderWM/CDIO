b6ebb3993878050a5a61e2dd7a131292e4cb4bd1
SimonChr <simonchristiansen1@gmail.com>
2017-05-04 03:06:56 +0200

    Account
    Account class updated
    comments added
    net worth is now changed for add and set balance methods

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -9,12 +9,21 @@ public class Account {
 	
 	DBconnector connector = new DBconnector();
 
+	/**
+	 * Account attributes
+	 */
 	int ID;
 	int money;
+	int networth;
 	
+	/**
+	 * Account constructor
+	 * @param Money and net worth predefined as 30000
+	 */
 	public Account(int ID){
 		this.ID = ID;
 		this.money = 30000;
+		this.networth = 30000;
 		try{
 		connector.doUpdate("game","INSERT into ACCOUNT values(" + ID + "," + 30000 + "," + 30000 + ");");
 		}
@@ -23,33 +32,54 @@ public class Account {
 			}
 	}
 	
+	/**
+	 * @return money
+	 */
 	public int getMoney(){
 		return money;
 	}
 	
+	/**
+	 * add a given amount of money to the balance and net worth
+	 */
 	public void addBalance(int money){
 		connector.Connect("game");
-		int newBalance = money + money;
+		int newBalance = this.money + money;
+		int newNetworth = networth + money;
 		money = newBalance;
+		networth = newNetworth;
 		try {
 			connector.doUpdate("Game","UPDATE ACCOUNT SET Money = " + newBalance + " WHERE PlayerID EQUALS " + ID + ");");
+			connector.doUpdate("Game","UPDATE ACCOUNT SET networth = " + newNetworth + " WHERE PlayerID EQUALS " + ID + ");");
 				connector.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * sets the balance to a specific amount
+	 * adds the difference to net worth
+	 */
 	public void setBalance(int balance){
+		int dif = balance - money;
+		networth = networth + dif;
 		money = balance;
 		connector.Connect("game");
 		try {
 			connector.doUpdate("Game","UPDATE ACCOUNT SET Money = " + balance + " WHERE PlayerID EQUALS " + ID + ");");
+			connector.doUpdate("Game","UPDATE ACCOUNT SET networth = " + networth + " WHERE PlayerID EQUALS " + ID + ");");
 				connector.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	
+	/**
+	 * @param Checks if the player has enough money to purchase something at a given price
+	 * @return boolean,  false if the player can't pay the price
+	 */
 	public boolean enoughMoney(int price){
 			return money > price;
 		
