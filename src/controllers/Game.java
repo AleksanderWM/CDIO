371cd59f070e49bc65604d226c4af5f24a12a180
Aleksander <aleksander.wm@gmail.com>
2017-05-05 02:31:04 +0200

    Commiting game

--- src/controllers/Game.java
+++ src/controllers/Game.java
@@ -2,17 +2,24 @@ package controllers;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
+import entities.Player;
 
 public class Game {
 static mGUI gui = new mGUI();
 GameBoard board = new GameBoard();
+public ArrayList<Player> playerList = new ArrayList<Player>();
 
-int numberOfPlayers = 0;
-PlayTurn thread = new PlayTurn();
+int numberOfPlayers = 3;
 public volatile int id = 1;
 
-	public void start(){
+	public void gameStart(){
+		board.CreateBoard();
+		gui.CreateBoard();
+
 		enterPlayers();
+		System.out.println(playerList.get(1).getName());
 	}
 
 	public void enterPlayers()
@@ -20,8 +27,7 @@ public volatile int id = 1;
 		while (numberOfPlayers < 2 && numberOfPlayers > 6)
 		{
 			//Message shown to user, to clarify that he needs to put in the correct value of players between 2 and 6.
-			
-			//code to mgui for entering an INT ammount
+			numberOfPlayers = gui.getUserInt("Enter Ammount of Players between 2 and 6");
 		}
 		
 		this.createPlayerThreads(numberOfPlayers);
@@ -35,12 +41,14 @@ public volatile int id = 1;
 	{
 		while (id != numberOfPlayers+1)
 		{
-		PlayTurn thread = new PlayTurn();
+		PlayTurn thread = new PlayTurn(id, this);
 		thread.run();
 		id++;
 		}
 
 	}
-	
+	public int gameId(){
+		return id;
+	}
 
 }
