54f6308025bdd48641c35b7acbdd18bbbf0b301e
SimonChr <simonchristiansen1@gmail.com>
2017-05-06 15:58:09 +0200

    ChanceCard
    ghuikl

--- src/entities/ChanceCard.java
+++ src/entities/ChanceCard.java
@@ -1,5 +1,29 @@
 package entities;
 
-public class ChanceCard {
+public abstract class ChanceCard {
 
+	protected int ChanceID;
+	protected int ChanceType;
+	protected String Description = null;
+	
+	public ChanceCard (int ID, int Type, String Des){
+		ChanceID = ID;
+		ChanceType = Type;
+		Description = Des;
+	}
+	
+	public abstract void Chance();
+	
+	public abstract int getChanceID();
+	
+	public abstract void setChanceID();
+	
+	public abstract int getChanceType();
+	
+	public abstract void setChanceType();
+	
+	public abstract String getDescription();
+	
+	public abstract void setDescription();
 }
+
