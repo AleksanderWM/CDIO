4646ad67a136ce289214ee6477845561bf932d0a
Aleksander <aleksander.wm@gmail.com>
2017-05-07 04:53:28 +0200

    PercentageTax and Railroad landonfield completed

--- src/entities/RailRoad.java
+++ src/entities/RailRoad.java
@@ -6,9 +6,15 @@ import controllers.Game;
 import controllers.GameBoard;
 import controllers.mGUI;
 
+
 public class RailRoad extends Ownable{
 
 	private Color TxColour = Color.BLACK;
+	private int ownedRailRoads = 0;
+	private int RENT_1 = 500;
+	private int RENT_2 = 1000;
+	private int RENT_3 = 2000;
+	private int RENT_4 = 4000;
 	
 	public RailRoad(String title, String subText, Color color, int player, int cost, int rent) {
 		super(title, subText, color, player, cost, rent);
@@ -17,6 +23,42 @@ public class RailRoad extends Ownable{
 
 	@Override
 	public void landOnField(Game game, GameBoard gameboard, int b, int p, mGUI mui, Shaker shake) {
+		if(((Ownable)gameboard.FieldList.get(b)).getOwner() != 10 && ((Ownable)gameboard.FieldList.get(b)).getOwner() != p){
+			int ownerRailRoads = ((Ownable)gameboard.FieldList.get(b)).getOwner();
+
+			if(ownerRailRoads == ((Ownable)gameboard.FieldList.get(7)).getOwner()){
+				ownedRailRoads++;
+			}
+			if(ownerRailRoads == ((Ownable)gameboard.FieldList.get(17)).getOwner()){
+				ownedRailRoads++;
+			}
+			if(ownerRailRoads == ((Ownable)gameboard.FieldList.get(27)).getOwner()){
+				ownedRailRoads++;
+			}
+			if(ownerRailRoads == ((Ownable)gameboard.FieldList.get(37)).getOwner()){
+				ownedRailRoads++;
+			}
+			game.playerList.get(p).getAccount().addBalance(-rent);
+			game.playerList.get(((Ownable)gameboard.FieldList.get(b)).getOwner()).getAccount().addBalance(rent);
+			
+		}
+	}
+	
+	public int getRent() 
+	{
+		int rent = 0;
+		switch(ownedRailRoads)
+		{
+		case 1: rent = RENT_1;
+		break;
+		case 2: rent = RENT_2;
+		break;
+		case 3: rent = RENT_3;
+		break;
+		case 4: rent = RENT_4;
+		break;
+		}
+		return rent;
 	}
 
 	@Override
